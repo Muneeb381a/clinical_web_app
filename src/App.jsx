b7a126926fa1e764de502364abd6a45a897befb5
Muneeb381a <muneebafzal381a@gmail.com>
2025-03-08 03:26:17 +0500

    Add print prescription button to PatientSearch component

--- src/App.jsx
+++ src/App.jsx
@@ -3670,7 +3670,7 @@ const PatientSearch = () => {
                                       ...item,
                                       dosage_en: e.value,
                                       dosage_urdu: e.label,
-                                    } 
+                                    }
                                   : item
                               )
                             );
@@ -3958,7 +3958,29 @@ const PatientSearch = () => {
                 </>
               )}
             </button>
+            <div className="mt-6">
+          <button
+            onClick={handlePrint}
+            className="bg-blue-600 hover:bg-blue-700 text-white font-medium py-2 px-4 rounded-lg transition-colors duration-200 flex items-center gap-2 print:hidden"
+            aria-label="Print prescription"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path
+                fillRule="evenodd"
+                d="M5 4v3H4a2 2 0 00-2 2v3a2 2 0 002 2h1v2a2 2 0 002 2h6a2 2 0 002-2v-2h1a2 2 0 002-2V9a2 2 0 00-2-2h-1V4a2 2 0 00-2-2H7a2 2 0 00-2 2zm8 0H7v3h6V4zm0 8H7v4h6v-4z"
+                clipRule="evenodd"
+              />
+            </svg>
+            Print Prescription
+          </button>
+        </div>
           </div>
+          
         ) : (
           showAddPatient && (
             <AddPatientForm
@@ -3967,6 +3989,8 @@ const PatientSearch = () => {
             />
           )
         )}
+
+        
       </div>
       <ToastContainer />
     </div>
