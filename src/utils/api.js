7690e8fa0ec49e8388141d411a02f793d6cefb31
Muneeb381a <muneebafzal381a@gmail.com>
2025-04-23 20:38:13 +0500

    feat(EditConsultation): update API endpoint to use production URL for consultations

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:4500";
+const BASE_URL = import.meta.env.VITE_API_BASE_URL || "https://patient-management-backend-nine.vercel.app";
 
 export const fetchWithRetry = async (
   method = "get",
